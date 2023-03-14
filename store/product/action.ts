0c4510c0c4eae8f0bbf2f91b5a8c4382420bcd67
maniruzzaman.akash <maniruzzaman.akash@paysera.net>
2023-03-14 10:16:16 +0600

    fix deals items

--- store/product/action.ts
+++ store/product/action.ts
@@ -65,8 +65,7 @@ export const getDealFlashListAction = () => (dispatch) => {
     }
     dispatch({type: Types.GET_FLASH_DEAL_DATA, payload: responseData});
     
-    // Axios.get(`get-items?type=deals-of-day&paginate_no=10`)
-    Axios.get(`get-items?paginate_no=20`)
+    Axios.get(`get-items?type=deals-of-day&paginate_no=10`)
         .then(res => {
             responseData.data = res.data.data.data;
             responseData.isLoading = false;
